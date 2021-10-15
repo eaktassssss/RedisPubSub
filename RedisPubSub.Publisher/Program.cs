463f5ec0e988f36406ef8c090b96fc2307c31e05
EVREN AKTAŞ <evren.aktas@outlook.com>
2021-10-16 01:40:11 +0300

    Add project files.

---
+++ RedisPubSub.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisPubSub.Publisher
+{
+    class Program
+    {
+        async static void Main(string[] args)
+        {
+            try
+            {
+                var channel = "message";
+                var connection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+                var pubsub = connection.GetSubscriber();
+                Console.WriteLine("Message:");
+                var message = Console.ReadLine();
+                await pubsub.PublishAsync(channel, message, CommandFlags.FireAndForget);
+                Console.WriteLine("Successful");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ RedisPubSub.Subscriber/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisPubSub.Subscriber
+{
+    class Program
+    {
+        async static void Main(string[] args)
+        {
+            try
+            {
+                var Channel = "message";
+                var connection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+                var pubsub = connection.GetSubscriber();
+                await pubsub.SubscribeAsync(Channel, (channel, message) => Console.WriteLine(message));
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
